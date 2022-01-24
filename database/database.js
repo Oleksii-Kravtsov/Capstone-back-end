82cff297d3177a4d216e58147b004dc7b0239f70
Zixin Zou <zxinmm@gmail.com>
2022-01-24 17:37:49 -0500

    connection to database

--- database/database.js
+++ database/database.js
@@ -1,10 +1,7 @@
 const Sequelize = require('sequelize')
 const env = require('dotenv').config()
 
-const sequelize = new Sequelize(process.env.DB_DATABSE, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-});
+const sequelize = new Sequelize(process.env.DB_DATABSE);
 
 
 module.exports = sequelize
