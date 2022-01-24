04508497d552240795addb2f8c917354235d573f
Zixin Zou <zxinmm@gmail.com>
2022-01-24 18:38:02 -0500

    change

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const env = require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_DATABSE, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging : false
 });
 
 
