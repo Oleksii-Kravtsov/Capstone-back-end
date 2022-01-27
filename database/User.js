9e5d7e2f73ea84eb96f1aa2c9f0cdd1334615582
Zixin Zou <zxinmm@gmail.com>
2022-01-26 23:33:49 -0500

    validation in User.js

--- database/User.js
+++ database/User.js
@@ -6,19 +6,33 @@ const User = db.define('user', {
     username: {
       type: sequelize.STRING,
       allowNull: false,
+      validate:{
+        notNull: {
+          msg : 'Please enter your email'
+        }
+      }
     },
     password: {
       type: sequelize.STRING,
       allowNull: false,
       validate:{
-        //check if password contain uppercase and lowercase and ...
+        check(password){
+          if (password.length<8&& !(/[a-z]/.test(password) && !(/[A-Z]/.test(password)) && !(/[0-9]/.test(password)))){
+            throw new Error(`\u2022 Password Length must be greater than 8 \u2022 include a Uppercase \u2022 include a Lowercase \u2022 include a Number`)
+          }
+        }
       }
     },
     email: {
         type: sequelize.STRING,
         allowNull: false,
         validate:{
-          isEmail: true,
+          notEmpty: {
+            msg : 'Please enter your email'
+          },  
+          isEmail:{
+            msg : 'Please enter a validate email'
+          }
         }
     },
     address: {
